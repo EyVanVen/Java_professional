62e242519fbce1776fa1ea83044c86ed19f402c9
EyVanVen <62552937+EyVanVen@users.noreply.github.com>
2021-04-21 14:55:33 +0300

    Add files via upload

---
+++ homework2/task2/Task2.java
@@ -0,0 +1,20 @@
+package homework2.task2;
+
+import java.util.*;
+
+public class Task2{
+    public static void main(String[] args) {
+
+        PriorityQueue <Integer> prq = new PriorityQueue<>();
+
+        for (int i = 0; i <= 10; i++){
+            prq.add((int) (Math.random() * 100));
+        }
+        System.out.println(prq);
+
+        while (!prq.isEmpty()){
+            System.out.println(prq.poll());
+        }
+
+    }
+}
